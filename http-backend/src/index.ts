e9b4b849e50046577cb17e91e65c5effca45450e
GUDLA UDAY SAI <udaysaigudla1111@gmail.com>
2025-09-08 22:03:56 +0530

    project setup

---
+++ http-backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express();
+
+app.post("/signup",(req,res)=>{
+
+
+
+})
+
+app.post("/signin",(req,res)=>{
+     
+})
+
+
+app.listen(3000,()=>{
+    console.log(`http-server is listening on port ${3000}`);
+})
