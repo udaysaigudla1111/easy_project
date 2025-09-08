98a4350a4570bc5b7dfa55bff01e47455709451f
GUDLA UDAY SAI <udaysaigudla1111@gmail.com>
2025-09-08 22:13:45 +0530

    Create 3endpoints in the http-backend

--- http-backend/src/index.ts
+++ http-backend/src/index.ts
@@ -4,14 +4,15 @@ const app = express();
 
 app.post("/signup",(req,res)=>{
 
-
-
 })
 
 app.post("/signin",(req,res)=>{
      
 })
 
+app.post("/create-room",(req,res)=>{
+    
+})
 
 app.listen(3000,()=>{
     console.log(`http-server is listening on port ${3000}`);
